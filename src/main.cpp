bc0daf8228e7de96c6030f602e1a92bfdecc4100
Roddy MacCrimmon <r.maccrimm@gmail.com>
2019-01-27 18:46:18 -0700

    Add command line options for input/output files

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,46 @@
 #include <iostream>
 #include <string>
 #include <compiler.h>
+#include <algorithm>
+
+const std::string usage_info = "Usage:\n\tplc src_file [-o output_file]";
 
 int main(int argc, char *argv[]) 
 {
-    std::ifstream file_in("test/error_test_program.pl");
-    if (!file_in.good()) {
-        std::cerr << "failed to open input file" << std::endl;
+    /*  Parse command line arguments
+    */
+    if (argc < 2) {
+        std::cout << "Missing source file" << std::endl << usage_info << std::endl;
         return -1;
     }
-    std::ofstream file_out("test/test_output");
+    std::string input_file(argv[1]);
+    std::string output_file = "scanner.out";
+
+    auto it = std::find(argv, argv + argc, std::string("-o"));
+    if (it != argv + argc) {
+        if (it == argv + (argc - 1)) {
+            std::cout << "-o option must be followed by output file" << std::endl 
+                      << usage_info << std::endl;
+            return -1;
+        }
+        output_file = std::string(*(it + 1));
+    }
+
+    /* Open input/output files
+    */
+    std::ifstream file_in(input_file);
+    if (!file_in.good()) {
+        std::cerr << "failed to open input file " + input_file << std::endl;
+        return -1;
+    }
+    std::ofstream file_out(output_file);
     if (!file_out.good()) {
-        std::cerr << "failed to open output file" << std::endl;
+        std::cerr << "failed to open output file " + output_file << std::endl;
         return -1;
     }
 
+    /* Compilation
+    */
     Compiler compiler(file_in);
     if (compiler.scan(file_out)) {
         std::cout << "Scan completed without errors." << std::endl;
