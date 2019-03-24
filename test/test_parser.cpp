db25009a4cf588efce39258c9136a82d055be089
Roderick MacCrimmon <rmaccrimm@gmail.com>
2019-03-24 00:40:12 -0600

    Add test case

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -23,7 +23,6 @@ Parser P;
 TEST_CASE("Variable redefinitions", "[var-defs]")
 {
     std::vector<Token> tlist = read_file("test/src_files/scope/var_redefined");
-    std::cout << "test/src_files/scope/var_redefined" << std::endl;
     REQUIRE(P.verify_syntax(&tlist) == 4);
 
 }
@@ -31,40 +30,41 @@ TEST_CASE("Variable redefinitions", "[var-defs]")
 TEST_CASE("Constant redefinitions", "[const-defs]") 
 {
     std::vector<Token> tlist = read_file("test/src_files/scope/const_redefined");
-    std::cout << "test/src_files/scope/const_redefined" << std::endl;
     REQUIRE(P.verify_syntax(&tlist) == 2);
 }
 
 TEST_CASE("Mixed redefinitions", "[all-defs]")
 { 
     std::vector<Token> tlist = read_file("test/src_files/scope/mixed_redefines");
-    std::cout << "test/src_files/scope/mixed_redefines" << std::endl;
     REQUIRE(P.verify_syntax(&tlist) == 4);
 }
 
 TEST_CASE("No redefinitions", "[all-defs-good]")
 { 
     std::vector<Token> tlist = read_file("test/src_files/scope/no_redefines");
-    std::cout << "test/src_files/scope/no_redefines" << std::endl;
     REQUIRE(P.verify_syntax(&tlist) == 0);
 }
 
 TEST_CASE("Array redefinitions", "[arr-defs]")
 { 
     std::vector<Token> tlist = read_file("test/src_files/scope/array_redefined");
-    std::cout << "test/src_files/scope/array_redefined" << std::endl;
     REQUIRE(P.verify_syntax(&tlist) == 2);
 }
 
 TEST_CASE("Procedure redefinitions", "[proc-defs]")
 { 
     std::vector<Token> tlist = read_file("test/src_files/scope/proc_redefined");
-    std::cout << "test/src_files/scope/proc_redefined" << std::endl;
     REQUIRE(P.verify_syntax(&tlist) == 3);
 }
+
 TEST_CASE("Procedure scope", "[proc-scope]")
 { 
-    tlist = read_file("test/src_files/scope/redefined_in_func");
-    std::cout << "test/src_files/scope/redefined_in_func" << std::endl;
+    std::vector<Token> tlist = read_file("test/src_files/scope/redefined_in_func");
     REQUIRE(P.verify_syntax(&tlist) == 3);
+}
+
+TEST_CASE("Array bounds type", "[array-bounds]")
+{
+    std::vector<Token> tlist = read_file("test/src_files/type/array_size");
+    REQUIRE(P.verify_syntax(&tlist) == 5);
 }
