81bf70d9f3e9257030a438e01b7dbff814c05ee1
Roderick MacCrimmon <rmaccrimm@gmail.com>
2019-03-24 23:04:17 -0600

    add missing test files

--- test/src_files/parser_scratch.pl
+++ test/src_files/parser_scratch.pl
@@ -1,7 +1,7 @@
 $PL Program: Linear Search
 begin
     const n = 10; integer array A[n];
-    integer x, i; Boolean found;
+
     proc Search
     begin integer m;
         i,m := 1,n;
@@ -10,7 +10,7 @@ begin
                 ~(A[i] = x) -> i := i+1;
             fi;
         od;
-        found := A[i] = x;
+
     end;
     $ Input table
     i := 1;
@@ -21,9 +21,18 @@ begin
     read x;
     do ~(x = 0) -> 
         call Search;
-        if found -> write x, i; []
+        
             ~found -> write x;
         fi;
         read x;
     od;
 end.
+
+begin
+end.
+
+                       
+
+
+
+                       
